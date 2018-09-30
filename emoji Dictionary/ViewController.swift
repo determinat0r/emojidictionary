422f89dd8afdeac1da104d02b547c4f35b55ec46
頼爽子 <determinat0r@yahoo.co.jp>
2018-09-30 11:24:52 +0900

    add array of emoji to tableview

--- emoji Dictionary/ViewController.swift
+++ emoji Dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var dacooltableview: UITableView!
     
+    var emojis = ["😀","😎","🦊","🍪","🏹"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +23,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😃"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
 
